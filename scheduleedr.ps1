a78d611ea6c222d6c9b23adb22bc062e71aec4c1
simon duchene <simon.duchene@gmail.com>
2023-08-10 08:50:39 -0700

    adding conti for immediate launch

--- scheduleedr.ps1
+++ scheduleedr.ps1
@@ -1,7 +1,24 @@
 $mypath = $MyInvocation.MyCommand.Path
 $path= Split-Path $mypath -Parent
+#first script to run now (in 1 hour) : conti
+  $date=(Get-date).AddHours(1) 
+  
+  write-output $date
+  
+  $action = New-ScheduledTaskAction -Execute "$path\edr0.ps1" 
+
+  $trigger =  New-ScheduledTaskTrigger -Once -At $date
+  $principal = New-ScheduledTaskPrincipal -UserID "$env:USERDOMAIN\$env:USERNAME" -LogonType S4U -RunLevel Highest 
+  Write-Output $action  $trigger $principal
+  
+  Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "edrattack0" -Description "edr0.ps1"  -Force
+ 
+
+
+#scripts to run in the next 9 months, 1 per month
+
 for ($num = 1 ; $num -le 9 ; $num++){    
-  $a=30*($num-1)
+  $a=30*($num)
   write-output $num
   write-output $a
   $date=(Get-date).AddDays($a)
