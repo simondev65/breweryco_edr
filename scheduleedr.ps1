f5eb88e6a75ea536bb5525444664cef5a87f631e
simon duchene <simon.duchene@gmail.com>
2023-08-16 15:17:53 -0700

    fixing ps1 powershell command

--- scheduleedr.ps1
+++ scheduleedr.ps1
@@ -5,11 +5,11 @@ $path= Split-Path $mypath -Parent
   
   write-output $date
   
-  $action = New-ScheduledTaskAction -Execute "$path\edr0.ps1" 
+  $action = New-ScheduledTaskAction C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Argument "-file $path\edr0.ps1" 
+ 
 
   $trigger =  New-ScheduledTaskTrigger -Once -At $date
-  $principal = New-ScheduledTaskPrincipal -UserID "$env:USERDOMAIN\$env:USERNAME" -LogonType S4U -RunLevel Highest 
-  Write-Output $action  $trigger $principal
+  $principal = New-ScheduledTaskPrincipal -UserID "$env:USERDOMAIN\$env:USERNAME"  -LogonType Password -RunLevel Highest
   
   Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "edrattack0" -Description "edr0.ps1"  -Force
  
